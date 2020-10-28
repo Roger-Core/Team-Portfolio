1271e5c9c8514f2615f5d40e6952bab767edab9f
Khay <altaircarn@gmail.com>
2020-10-27 19:16:29 -0600

    APALVER

--- src/Navbar/Sidedrawer/index.js
+++ src/Navbar/Sidedrawer/index.js
@@ -1,46 +1,49 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-import "./Sidedrawer.scss";
+import './Sidedrawer.scss';
 
 const Index = (props) => {
-  let drawerClasses = "side-drawer";
+	let drawerClasses = 'side-drawer';
 
-  if (props.show) {
-    drawerClasses = "side-drawer open";
-  }
+	if (props.show) {
+		drawerClasses = 'side-drawer open';
+	}
 
-  return (
-    <nav className={drawerClasses}>
-      <ul>
-        <h2
-          style={{ marginBottom: "25px", marginLeft: "35px", color: "#22ce78" }}
-        >
-          MENU
-        </h2>
-        <Link to="/">
-          <i className="fa fa-home" aria-hidden="true"></i>
-          <li>Home</li>
-          <hr />
-        </Link>
-        <Link to="/">
-          <i class="fas fa-users" aria-hidden="true"></i>
-          <li>About</li>
-          <hr />
-        </Link>
-        <Link to="/">
-          <i className="fas fa-briefcase" aria-hidden="true"></i>
-          <li>Portfolio</li>
-          <hr style={{ color: "#22ce78" }} />
-        </Link>
-        <Link to="/">
-          <i class="fas fa-address-card" aria-hidden="true"></i>
-          <li>Contact</li>
-          <hr />
-        </Link>
-      </ul>
-    </nav>
-  );
+	return (
+		<nav className={drawerClasses}>
+			<ul>
+				<h2
+					style={{
+						marginBottom: '25px',
+						marginLeft: '35px',
+						color: '#22ce78',
+					}}>
+					MENU
+				</h2>
+				<a href='#home'>
+					<i className='fa fa-home' aria-hidden='true'></i>
+					<li>Home</li>
+					<hr />
+				</a>
+				<a href='#portfolio'>
+					<i className='fas fa-briefcase' aria-hidden='true'></i>
+					<li>Portfolio</li>
+					<hr />
+				</a>
+				<a href='#about'>
+					<i className='fas fa-users' aria-hidden='true'></i>
+					<li>About</li>
+					<hr style={{ color: '#22ce78' }} />
+				</a>
+				<a href='#technologies'>
+					<i className='fas fa-address-card' aria-hidden='true'></i>
+					<li>Technologies</li>
+					<hr />
+				</a>
+			</ul>
+		</nav>
+	);
 };
 
 export default Index;
--- src/Routes/Home/index.js
+++ src/Routes/Home/index.js
@@ -1,33 +1,34 @@
-import React from "react";
-import DevsInfo from "../../DevsInfo";
-import TechIcons from "../TechIcons";
+import React from 'react';
+import TechIcons from '../TechIcons';
 
-import "./Home.scss";
+import './Home.scss';
 
-import Projects from "../../Projects";
+import Projects from '../../Projects';
 
 const Home = () => {
-  return (
-    <section>
-      <div className="presentation">
-        <h1>{"<Hello World />"}</h1>
-        <br />
-        <div className="story">
-          <h4>We will work together for your creative project.</h4>
-        </div>
-      </div>
-      <Projects />
-      <div className="col-md-12 text-center">
-        <h3>Developers Team</h3>
-        <p className="about-us">
-          Our team is highly trained and updated on new trends in web page
-          design <br /> to meet the most demanding needs of customers.
-        </p>
-        <DevsInfo />
-        <TechIcons />
-      </div>
-    </section>
-  );
+	return (
+		<div className='main'>
+			<div id='home' className='presentation'>
+				<h1>{'<Hello World />'}</h1>
+				<br />
+				<div className='story'>
+					<h4>We will work together for your creative project.</h4>
+				</div>
+			</div>
+			<Projects />
+			<div className='col-md-12 text-center'>
+				<h3>Developers Team</h3>
+				<p id='about' className='about-us'>
+					Our team is highly trained and updated on new trends in web page
+					design <br /> to meet the most demanding needs of customers.
+					<br /> Contact us for any job: <strong> bedevs.mex@gmail.com</strong>
+				</p>
+				<h3 id='technologies'>Technologies</h3>
+				<p></p>
+				<TechIcons />
+			</div>
+		</div>
+	);
 };
 
 export default Home;
